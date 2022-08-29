2938915430a4656e1ac12ec9c8414c65da41d9c9
HumzaTAhmad <humza3173@gmail.com>
2022-08-29 00:20:48 -0400

    update hashtable to have chaining
    If two duplicates values have the same hash function we don't want
    overwrites. So I implemented chaining so multiple values can have the
    same hash function.

--- data-structures/hashtable.py
+++ data-structures/hashtable.py
@@ -2,30 +2,47 @@
 class HashTable:
     def __init__ (self):
         self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.arr = [[] for i in range(self.MAX)]
     
     def get_hash(self, key):
         h=0
         for char in key:
             h += ord(char)
-        return h % self.MAX
+        return h % 10
     
     def __setitem__(self, key, value):
         h = self.get_hash(key)
-        self.arr[h] = value
+
+        found = False
+        for index, element in enumerate(self.arr[h]):
+            if len(element)==2 and element[0]==key:
+                self.arr[h][index] = (key, value)
+                found = True
+                break
+        if not found:
+            self.arr[h].append((key, value))
+        
 
     def __getitem__(self, key):
         h = self.get_hash(key)
-        return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+        
 
     def __delitem__(self, key):
         h = self.get_hash(key)
-        self.arr[h] = None
+        for index, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                del self.arr[h][index]
+
 
 class main: 
     t = HashTable()
     t["march 6"] = 130
-    t["march 1"] = 20
-    t["dec 17"] = 49
-    del t['march 6']
+    t["march 17"] = 5
+    print(t["march 6"])
+    print(t["march 17"])
+    del t["march 17"]
     print(t.arr)
+  
